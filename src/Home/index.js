450ea0f285d8559101e485c219fcba542ff99305
hyunsoo.chang <zenith11311@gmail.com>
2023-04-06 21:47:39 +0900

    fix: 🐛 init App -> Pages&Component

---
+++ src/Home/index.js
@@ -0,0 +1,18 @@
+import './index.css';
+
+
+const Home = () => {
+    return (
+        <div className="container p-2">
+            <div className="row">
+                <div className="col-12">
+                    <div className="d-flex justify-content-center">
+                        <div className="p-3 bg-dark" style={{ display:"inline-block" }}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
---
+++ src/_Components/ProfileCard.js
@@ -0,0 +1,12 @@
+const ProfileCard  = () => {
+    return (
+        <>
+            <div className="d-flex justify-content-center">
+                <div className="p-5 bg-dark">
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default ProfileCard;
