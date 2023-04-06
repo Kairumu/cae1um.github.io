13beeeed9682099a61e6f025914f058bc55cc7de
hyunsoo.chang <zenith11311@gmail.com>
2023-04-06 23:28:53 +0900

    fix: 🐛 removed nfra from url

--- src/Work/Data.js
+++ src/Work/Data.js
@@ -1,70 +1,9 @@
-const sides = [
-    { 
-        name : "int-dev",
-        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/int-devel.png?alt=media&token=dbea711e-b8c1-4b1d-808e-252591abb922",
-        link : "https://int-devel.web.app/",
-        role : "#Design #Develop",
-        tech : "#react-js #canvas",
-        desc : "Playing with canvas",
-
-    },
-    { 
-        name : "pic_around",
-        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/pic_around.jpg?alt=media&token=5e8799ac-2c1b-4859-a937-1dc04327f046",
-        link : "http://csmsk8x2004.nfra.io:7777/",
-        role : "#Design #Develop",
-        tech : "#flask #jquery #clous",
-        desc : "Show user popular contents(imgs) around them, help user choose right POI for the moment",
-
-    },
-    { 
-        name : "ourmap",
-        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/ourmap.jpg?alt=media&token=d14ee313-7b03-412d-9610-927b01de230d",
-        link : "http://csmsk8x2004.nfra.io:7003/home", 
-        role : "#Develop", 
-        tech : "#flask #jquery #elasticsearch",
-        desc : `Collaborative poi bookmark service... 
-                Multiple users can participate making maps...`,
-    },
-    { 
-        name : "What to Eat v2",
-        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/%E1%84%86%E1%85%AF%E1%84%86%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%B3%E1%86%AF%E1%84%81%E1%85%A1.jpg?alt=media&token=9211f5fe-1546-4ebb-a378-2c5116e2b4cf",
-        link : "http://asca0x1311.nfra.io:3004/", 
-        role : "#DataRefine #DataServing #Prototype", 
-        tech : "#flask #jquery",
-        desc : "Internal project, menu based recommend service",
-    },
-    { 
-        name : "What to Eat",
-        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/%E1%84%86%E1%85%AF%E1%84%86%E1%85%A5%E1%86%A8%E1%84%8C%E1%85%B5.jpg?alt=media&token=c652219d-cb2e-4d29-8956-d5d216d14b66",
-        link : "http://asca0x1311.nfra.io:4004/", 
-        role : "#DataRefine #DataServing #Prototype", 
-        tech : "#flask #jquery",
-        desc : "Internal project, menu based recommend service",
-    },
-    { 
-        name : "ppopgi.io", 
-        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/ppopgi.jpg?alt=media&token=570c9caa-cb5f-4637-b272-d84c1afd6d5c",
-        link : "https://ppopgi.io/",
-        role : "#Design #Develop",
-        tech : "#react.js #firebase",
-        desc : 'Drawing user defined lots...',
-    },
-    { 
-        name : "Résumé",
-        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/Resume.jpg?alt=media&token=347ed07b-9911-47eb-95a8-ae7b5a8b4293",
-        link : "https://resume-db14c.web.app/",
-        role : "#Design #Develop",
-        tech : "#react.js #firebase",
-        desc : "Simple résumé service...",
-    },
-];
-
 const naver = [
     { 
         name : "Local DataPipe for Smartblock",
         img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/smartblock.png?alt=media&token=5386d236-d0d9-4518-a677-56cb3768fc93", 
-        link : "https://m.search.naver.com/search.naver?query=%EB%B6%80%EC%82%B0+%EC%97%AC%ED%96%89",
+        link : "asca0x1311",
+        //link : "https://m.search.naver.com/search.naver?query=%EB%B6%80%EC%82%B0+%EC%97%AC%ED%96%89",
         role : "#DataRefine #DataServing", 
         tech : "#spark #clous",
         desc : `Data pipeline for Search service`
@@ -89,7 +28,7 @@ const naver = [
     {
         name : "PICK for Light Users", 
         img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/light_pick.jpg?alt=media&token=1b4c62bd-8ba7-4a78-9d6a-2603c54bca6a",
-        link : "http://asca0x1311.nfra.io:3000/?rcode=09680",
+        link : "asca0x1311",
         role : "#DataRefine #DataServing #Prototype", 
         tech : "#spark #clous #Lambda #react.js",
         desc : "POI recommendation by gender-age group for NAVER users",
@@ -113,7 +52,7 @@ const naver = [
     { 
         name : "C3S Fileviewer",
         img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/c3s_fv.jpg?alt=media&token=99b24a88-6ec6-4585-8b47-a8cce3e8533b",
-        link : "http://asca0x1311.nfra.io:3003/?path=hdfs%3A%2F%2Fcamino%2Fuser%2Fsmartaround-modeling%2F", 
+        link : "asca0x1311", 
         role : "#Design #Develop",
         tech : "#flask #react.js #hdfs",
         desc : `Utility service used for browsing, viewing files in internal hdfs service,
@@ -158,5 +97,66 @@ const tmax = [
     },
 ];
 
+const sides = [
+    { 
+        name : "int-dev",
+        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/int-devel.png?alt=media&token=dbea711e-b8c1-4b1d-808e-252591abb922",
+        link : "https://int-devel.web.app/",
+        role : "#Design #Develop",
+        tech : "#react-js #canvas",
+        desc : "Playing with canvas",
+
+    },
+    { 
+        name : "pic_around",
+        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/pic_around.jpg?alt=media&token=5e8799ac-2c1b-4859-a937-1dc04327f046",
+        link : "csmsk8x2004",
+        role : "#Design #Develop",
+        tech : "#flask #jquery #clous",
+        desc : "Show user popular contents(imgs) around them, help user choose right POI for the moment",
+
+    },
+    { 
+        name : "ourmap",
+        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/ourmap.jpg?alt=media&token=d14ee313-7b03-412d-9610-927b01de230d",
+        link : "csmsk8x2004", 
+        role : "#Develop", 
+        tech : "#flask #jquery #elasticsearch",
+        desc : `Collaborative poi bookmark service... 
+                Multiple users can participate making maps...`,
+    },
+    { 
+        name : "What to Eat v2",
+        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/%E1%84%86%E1%85%AF%E1%84%86%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%B3%E1%86%AF%E1%84%81%E1%85%A1.jpg?alt=media&token=9211f5fe-1546-4ebb-a378-2c5116e2b4cf",
+        link : "asca0x1311", 
+        role : "#DataRefine #DataServing #Prototype", 
+        tech : "#flask #jquery",
+        desc : "Internal project, menu based recommend service",
+    },
+    { 
+        name : "What to Eat",
+        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/%E1%84%86%E1%85%AF%E1%84%86%E1%85%A5%E1%86%A8%E1%84%8C%E1%85%B5.jpg?alt=media&token=c652219d-cb2e-4d29-8956-d5d216d14b66",
+        link : "asca0x1311", 
+        role : "#DataRefine #DataServing #Prototype", 
+        tech : "#flask #jquery",
+        desc : "Internal project, menu based recommend service",
+    },
+    { 
+        name : "ppopgi.io", 
+        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/ppopgi.jpg?alt=media&token=570c9caa-cb5f-4637-b272-d84c1afd6d5c",
+        link : "https://ppopgi.io/",
+        role : "#Design #Develop",
+        tech : "#react.js #firebase",
+        desc : 'Drawing user defined lots...',
+    },
+    { 
+        name : "Résumé",
+        img  : "https://firebasestorage.googleapis.com/v0/b/potfolio-ebeda.appspot.com/o/Resume.jpg?alt=media&token=347ed07b-9911-47eb-95a8-ae7b5a8b4293",
+        link : "https://resume-db14c.web.app/",
+        role : "#Design #Develop",
+        tech : "#react.js #firebase",
+        desc : "Simple résumé service...",
+    },
+];
 
 export { sides, naver, tmax, };
